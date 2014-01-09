9be40cc237e44f9b50d960e2339b49df3eb6f243
Tomasz Wójcik <tomasz.prgtw.wojcik+github@gmail.com>
2014-01-09 18:58:28 +0100

    Update 02-install-basic-stuff.sh

--- update/02-install-basic-stuff.sh
+++ update/02-install-basic-stuff.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-apt-get install --yes --force-yes build-essential atop htop mytop nano subversion git pv
+apt-get update
+apt-get install --yes --force-yes build-essential atop htop mytop nano subversion git pv guake
