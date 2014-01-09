0f32aa0c8b6b67ff436fb3ba161f5eaea74bb233
Tomasz Wójcik <tomasz.prgtw.wojcik+github@gmail.com>
2014-01-09 23:26:21 +0100

    Update 10.symlink-dotfiles.sh

--- install/10.symlink-dotfiles.sh
+++ install/10.symlink-dotfiles.sh
@@ -2,7 +2,7 @@
 
 
 dotfiles="$(readlink -f ../dotfiles)"
-for dotfile in `ls -A "$dotfiles"`; do
+for dotfile in `ls -A "$dotfiles/*"`; do
     #[[ -f "$HOME/$file" ]] && echo mv "$HOME/$file" "$HOME/$file.bak"
     echo ln -f -s $dotfile "$HOME/$(basename $dotfile)"
 done
