fd4b7b62ed2184289838208ec483c7f7f8eefa90
Tomasz Wójcik <tomasz.prgtw.wojcik+github@gmail.com>
2014-01-09 23:18:15 +0100

    Update 10.symlink-dotfiles.sh

--- install/10.symlink-dotfiles.sh
+++ install/10.symlink-dotfiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-dir="$(dirname $0)/../dotfiles"
-echo "DIR: "$dir
+echo "ASDA"
+#dir="$(dirname $0)/../dotfiles"
+#echo "DIR: "$dir
 #for file in `ls -A "$dir"`; do
     #[[ -f "$HOME/$file" ]] && echo mv "$HOME/$file" "$HOME/$file.bak"
     #echo ln -f -s $dir/$file "$HOME/$(basename $file)"
