f69a43d4ea1f9a16621ed8806fbafd561ab1ab9a
Tomasz Wójcik <tomasz.prgtw.wojcik+github@gmail.com>
2014-01-09 20:22:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,10 @@ color()
 dir="$HOME/dotfiles"
 mkdir -p $dir
 if [ "`ls -A $dir`" ]; then
-    echo "Cannot install, $dir is not empty"
+    for file in install/*.sh; do
+        color "Running "$file
+        bash $file
+    done
 else
     cd $dir
     git clone git://github.com/prgTW/dotfiles.git .
