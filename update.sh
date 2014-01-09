5696592f267db788d2ff815f1e63c532c3f2683e
Tomasz Wójcik <tomasz.prgtw.wojcik+github@gmail.com>
2014-01-09 19:02:44 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,16 +2,7 @@
 
 pushd .
 
-color()
-{
-    echo -e "\e[01;32m >> \e[00m$*";
-}
-
 cd `dirname $BASH_SOURCE`
-for file in /update/*.sh
-do
-    color "Running "$file
-    bash $file
-done
+git pull
 
 popd
