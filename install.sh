a3f8265207c8ae0d4cd1478828fe00cd646a90f9
Tomasz Wójcik <tomasz.prgtw.wojcik+github@gmail.com>
2014-01-11 19:44:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,60 @@
 #!/bin/bash
 
-if [ "$1" -neq "desktop"] and [ "$1" -neq "server" ] ; then
-    echo Specify "desktop" or "server" as the first parameter
-    return 1
-fi
-
-isDesktop=[[ "$1" == "desktop" ]]
-isServer=[[ "$1" == "server" ]]
-
 color()
 {
     echo -e "\e[01;32m$*\e[00m";
 }
 
-sudo apt-get update
-sudo apt-get --yes --force-yes install git
+prepare()
+{
+    sudo apt-get update
+    sudo apt-get --yes --force-yes install git
+    
+    curdir=`pwd`
+    dir="$HOME/dotfiles"
+    mkdir -p $dir
+    cd $dir
+    
+    if [ "`ls -A $dir`" == "" ]; then
+        git clone git://github.com/prgTW/dotfiles.git .
+    else
+        git reset --hard HEAD
+        git pull
+    fi
+    
+    cd $curdir
+}
 
-curdir=`pwd`
-dir="$HOME/dotfiles"
-mkdir -p $dir
-cd $dir
+process_file()
+{
+    color "Running "$1
+    source $1
+}
 
-if [ "`ls -A $dir`" == "" ]; then
-    git clone git://github.com/prgTW/dotfiles.git .
-else
-    git reset --hard HEAD
-    git pull
+bootstrap()
+{
+    for file in `ls -A bootstrap/$1/*.sh`; do
+        process_file $file
+    done
+}
+
+install()
+{
+    bootstrap general
+    bootstrap $1
+}
+
+prepare
+
+if [ "$1" -neq "desktop"] and [ "$1" -neq "server" ] ; then
+    echo Specify "desktop" or "server" as the first parameter
+    return 1
 fi
 
-for file in `ls -A install/*.sh`; do
-    color "Running "$file
-    source $file
-done
+if [ "$1" -neq "desktop"] and [ "$1" -neq "server" ] ; then
+    install server
+fi
 
-cd $curdir
+if [ "$1" -neq "desktop"] and [ "$1" -neq "server" ] ; then
+    install desktop
+fi
