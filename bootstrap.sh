8c21b207412cb9198f286d82b5eb335f1c473e6e
Tomasz Wójcik <tomasz.prgtw.wojcik+github@gmail.com>
2014-01-11 21:25:07 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+bootstrapName=
+
 color()
 {
     echo -e "\e[01;32m$*\e[00m";
@@ -8,7 +10,7 @@ color()
 process_file()
 {
     color "Running "$1
-    source $1
+    source $1 "$bootstrapName"
 }
 
 bootstrap()
